4fbc434f71adc010ed610fcfcccf6bc2b2b65457
Lukasz Tackowiak <lukasz.tackowiak@polcode.com>
2016-08-11 15:38:54 +0200

    Example of how to show user rewards in Sample app.

--- sample/app/src/main/java/com/useek/sdksample/MainActivity.java
+++ sample/app/src/main/java/com/useek/sdksample/MainActivity.java
@@ -7,18 +7,23 @@ import android.view.View;
 import com.useek.sdk.UseekSDK;
 
 public class MainActivity extends AppCompatActivity {
+    UseekSDK sdk = new UseekSDK(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sdk.setPublisherId("5a857cb7958adcf9a1f53239b0c2eced");
+        sdk.setUserId("lukasz-tackowiak");
     }
 
     public void openPlayer(View view) {
-        UseekSDK sdk = new UseekSDK(this);
-        sdk.setPublisherId("3543049ba603c93461697c43aa71fafd");
-        sdk.setUserId("external-user-id");
-        sdk.setVideoId(125);
+        sdk.setVideoId(1);
         sdk.playVideo();
+    }
+
+    public void showRewards(View view) {
+        sdk.showRewards();
     }
 }
