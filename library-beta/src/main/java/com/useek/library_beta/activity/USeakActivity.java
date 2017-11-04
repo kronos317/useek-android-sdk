daa68a947a361fac009cf0df85f87e103487263f
Chris Lin <wuqian@toptal.com>
2017-11-04 14:32:11 +0800

    add library classes

---
+++ library-beta/src/main/java/com/useek/library_beta/activity/USeakActivity.java
@@ -0,0 +1,15 @@
+package com.useek.library_beta.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.useek.library_beta.R;
+
+public class USeakActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_useak);
+    }
+}
---
+++ library-beta/src/main/java/com/useek/library_beta/manager/USeakUtils.java
@@ -0,0 +1,8 @@
+package com.useek.library_beta.manager;
+
+/**
+ * Created by Chris Lin on 11/4/2017.
+ */
+
+public class USeakUtils {
+}
